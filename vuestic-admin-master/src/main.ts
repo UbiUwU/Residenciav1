5221a13eab65bf04d10e97cf063c7fd020c8855d
UbiUwU <keflok23@gmail.com>
2025-04-09 15:52:29 -0500

    La sesion se mantiene guardado.

--- vuestic-admin-master/src/main.ts
+++ vuestic-admin-master/src/main.ts
@@ -10,12 +10,17 @@ import stores from './stores'
 import router from './router'
 import vuesticGlobalConfig from './services/vuestic-ui/global-config'
 
+import { useAuthStore } from './stores/auth'
+
 const app = createApp(App)
 
 app.use(stores)
 app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
+
+const authStore = useAuthStore()
+authStore.initialize()
 
 if (import.meta.env.VITE_APP_GTM_ENABLED) {
   app.use(
