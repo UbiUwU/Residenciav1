afd281c8306a466c5d8d02b194ea7a4f5dd54b25
AndresMukulCab <andresmc4142@gmail.com>
2025-04-09 15:45:14 -0500

    Agregacion de imagen de Login

--- vuestic-admin-master/src/layouts/AuthLayout.vue
+++ vuestic-admin-master/src/layouts/AuthLayout.vue
@@ -2,12 +2,19 @@
   <VaLayout v-if="breakpoint.lgUp" class="h-screen bg-[var(--va-background-secondary)]">
     <template #left>
       <RouterLink
-        class="bg-primary h-full flex items-center justify-center"
+        class="h-full relative block overflow-hidden"
         style="width: 35vw"
         to="/"
         aria-label="Visit homepage"
       >
-        <VuesticLogo :height="28" start="#FFF" />
+        <!-- Imagen de fondo que cubre todo el espacio -->
+        <img 
+          src="../img/Login.jpg" 
+          alt="Background" 
+          class="absolute inset-0 w-full h-full object-cover"
+        />
+        <!-- Logo/Marca opcional centrado sobre la imagen -->
+        
       </RouterLink>
     </template>
     <template #content>
@@ -23,7 +30,7 @@
         <main class="h-full flex flex-row items-center justify-start mx-auto max-w-[420px]">
           <div class="flex flex-col items-start">
             <RouterLink class="py-4" to="/" aria-label="Visit homepage">
-              <VuesticLogo class="mb-2" start="#0E41C9" />
+              <img src="../img/Login.jpg" alt="Logo" class="h-10 mb-2 object-contain" />
             </RouterLink>
             <RouterView />
           </div>
@@ -35,7 +42,6 @@
 
 <script lang="ts" setup>
 import { useBreakpoint } from 'vuestic-ui'
-import VuesticLogo from '../components/VuesticLogo.vue'
 
 const breakpoint = useBreakpoint()
 </script>
