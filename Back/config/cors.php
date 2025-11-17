e721642ae717a382824969b8b0fa664d12dc00de
UbiUwU <keflok23@gmail.com>
2025-11-17 13:44:42 -0500

    inicio en linux

--- Back/config/cors.php
+++ Back/config/cors.php
@@ -3,11 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-<<<<<<< HEAD
-    'allowed_origins' => ['http://localhost:5173'], 
-=======
     'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'], 
->>>>>>> 50f1347702246fae5a2ded8433859d2dd8f2f12c
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
